231011196f3003f5be0f5a9820ac1ea86397b96e
JuWon Kim <kimproweb101@gmail.com>
2024-08-24 11:45:37 +0900

    s17-complete-filter

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -7,22 +7,29 @@
       {{ postItem.contents }}
     </div>
     <div class="post-time">
-      {{ postItem.createdAt }}      
+      {{ filteredDate(postItem.createdAt) }}      
       <i class="icon ion-md-create" @click="routeEditPage(postItem._id)"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>      
     </div>
   </li>
 </template>
 <script>
+import { formatDate } from '@/utils/filters.js'
 import { deletePost } from '@/api/posts.js';
-export default{
+export default {
   props:{
     postItem:{
       type:Object,
       required:true,
     }
+  },  
+  computed:{
+    
   },
   methods : {
+    filteredDate(_date) {
+      return formatDate(_date)
+    },
     async deleteItem(){
       if(confirm('You want to delete it?')){
         try {
--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import './css/reset.css'
 import './css/common.css'
 import { createApp } from 'vue'
-
 import { store } from './store/index'
-
 
 
 import App from './App.vue'
 import router from './router'
-
-
 
 const app = createApp(App)
 
